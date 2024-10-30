8bab355511d34f35e9f727fdaea4d953fb18d08e
sebastiancavero <axelcaverocontacto@gmail.com>
2024-10-30 04:45:27 -0500

    no sleep push

--- src/components/BuildingModel.jsx
+++ src/components/BuildingModel.jsx
@@ -8,9 +8,10 @@ const Floors = ({ floorPositions, activeMeshIndex, handleClick }) => {
         const yOffset =
           floorPositions
             .slice(0, index)
-            .reduce((acc, currentFloor) => acc + currentFloor.args[1], 0)
+            .reduce((acc, currentFloor) => acc + currentFloor.args[1], 4.925)
 
         return (
+          
           <mesh
             key={index}
             position={[floor.position[0], yOffset, floor.position[2]]}
@@ -18,6 +19,7 @@ const Floors = ({ floorPositions, activeMeshIndex, handleClick }) => {
             onClick={(e) => {
               e.stopPropagation()
               handleClick(index)
+              
             }}
           >
             <boxGeometry args={floor.args} />
@@ -34,7 +36,7 @@ const Floors = ({ floorPositions, activeMeshIndex, handleClick }) => {
 }
 
 const BuildingModel = (props) => {
-  const { scene } = useGLTF('/models/EDIFICIO/prueba total 1- centrao.glb')
+  const { scene } = useGLTF('/models/EDIFICIO/edificio_final.glb')
 
   useEffect(() => {
     scene.traverse((child) => {
@@ -47,107 +49,107 @@ const BuildingModel = (props) => {
 
   const floorPositions = [
     {
-      position: [0, 0, -1],
+      position: [0.127, 1, -0.015],
       // primer argumento es el ancho
       // segundo argumento es el alto
       // tercer argumento es el largo
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      args: [.152, 0.095, .375],
+      rotation: [0, 0, 0],
     }, // Primer piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.015],
+      args: [.152, 0.0276525, .375],
+      rotation: [0, 0, 0],
     }, // Segundo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.015],
+      args: [.152, 0.0276525, .375],
+      rotation: [0, 0, 0],
     }, // Tercer piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.015],
+      args: [.152, 0.0276525, .375],
+      rotation: [0, 0, 0],
     }, // Cuarto piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.015],
+      args: [.152, 0.0276525, .375],
+      rotation: [0, 0, 0],
     }, // Quinto piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.015],
+      args: [.152, 0.0276525, .375],
+      rotation: [0, 0, 0],
     }, // Sexto piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.015],
+      args: [.152, 0.0276525, .375],
+      rotation: [0, 0, 0],
     }, // Séptimo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.015],
+      args: [.152, 0.0276525, .375],
+      rotation: [0, 0, 0],
     }, // Octavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.015],
+      args: [.152, 0.0276525, .375],
+      rotation: [0, 0, 0],
     }, // Noveno piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Décimo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Onceavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Doceavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Treceavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Catorceavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Quinceavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Dieciseisavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Diecisieteavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Dieciochoavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.152, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Diecinueveavo piso
     {
-      position: [0, 0, -1],
-      args: [.1, 1, .1],
-      rotation: [0, Math.PI / -8.5, 0],
+      position: [0.127, 1, -0.034],
+      args: [.16, 0.0276525, .35],
+      rotation: [0, 0, 0],
     }, // Veinteavo piso
   ];
 
