630da8a1801570e3a9a8e26289ca5614aca5c6e0
sebastiancavero <axelcaverocontacto@gmail.com>
2024-12-23 20:16:10 -0500

    new city update

--- src/data/modelPaths.js
+++ src/data/modelPaths.js
@@ -1,6 +1,6 @@
 // Archivo: modelPaths.js
 const modelPaths = {
-  edificio: '/models/EDIFICIO/prueba-151124.glb',
+  edificio: '/models/EDIFICIO/prueba2.glb',
   't-1': '/models/DEPARTAMENTOS/TIPO-1.glb',
   't-2': '/models/DEPARTAMENTOS/TIPO-2.glb',
   't-3': '/models/DEPARTAMENTOS/TIPO-3.glb',
