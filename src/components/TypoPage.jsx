c1ef7781da24adadc835c8f6c9764746c2e2d229
sebastiancavero <axelcaverocontacto@gmail.com>
2024-12-23 22:36:36 -0500

    typopage reupdate

--- src/components/TypoPage.jsx
+++ src/components/TypoPage.jsx
@@ -1,38 +1,44 @@
-import React, { useState, useEffect, Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import CameraController from './CameraController';
-import Model from './Model';
-import GlobalRotateIcon from './icons/GlobalRotateIcon';
-import ZoomInIcon from './icons/ZoomInIcon';
-import ZoomOutIcon from './icons/ZoomOutIcon';
-import { useNavigate } from 'react-router-dom';
-import AnimatedButton from './AnimatedButton';
-import * as THREE from 'three';
-import ReturnIcon from './icons/ReturnIcon';
-import BuildingIcon from './icons/BuildingIcon';
-import View3dIcon from './icons/View3dIcon';
-import Hide3dIcon from './icons/Hide3dIcon';
-import CameraUpIcon from './icons/CameraUpIcon';
-import CameraDownIcon from './icons/CameraDownIcon';
-import { Environment } from '@react-three/drei';
-import FocusIcon from './icons/FocusIcon';
+import React, { useState, useEffect, Suspense, useRef } from 'react'
+import { Canvas } from '@react-three/fiber'
+import CameraController from './CameraController'
+import Model from './Model'
+import GlobalRotateIcon from './icons/GlobalRotateIcon'
+import ZoomInIcon from './icons/ZoomInIcon'
+import ZoomOutIcon from './icons/ZoomOutIcon'
+import { useNavigate } from 'react-router-dom'
+import AnimatedButton from './AnimatedButton'
+import * as THREE from 'three'
+import ReturnIcon from './icons/ReturnIcon'
+import BuildingIcon from './icons/BuildingIcon'
+import View3dIcon from './icons/View3dIcon'
+import Hide3dIcon from './icons/Hide3dIcon'
+import CameraUpIcon from './icons/CameraUpIcon'
+import CameraDownIcon from './icons/CameraDownIcon'
+import { Environment } from '@react-three/drei'
+import FocusIcon from './icons/FocusIcon'
+import TerrazaCameraController from './TerrazaCameraController'
 
-function TypoPage({ activeModel, isLoaded, activeTypology }) {
-  const [rotation, setRotation] = useState(0);
-  const [zoom, setZoom] = useState(1);
-  const [isAnimationTriggered, setIsAnimationTriggered] = useState(false);
-  const [isReverseAnimationTriggered, setIsReverseAnimationTriggered] = useState(false);
-  const [stateView, setStateView] = useState([Math.PI / 2, 0, 0]);
-  const [isToggleActive, setIsToggleActive] = useState(false);
-  const [isImageOpen, setIsImageOpen] = useState(false);
-  const [resetPosition, setResetPosition] = useState(false);
-  const [cameraPosition, setCameraPosition] = useState([0, 0, 5]);
+function TypoPage ({ activeModel, isLoaded, activeTypology }) {
+  const cameraRef = useRef()
+  const [rotation, setRotation] = useState(0)
+  const [zoom, setZoom] = useState(70)
+  const [zoom2, setZoom2] = useState(0.8)
+  const [rotateFront, setRotateFront] = useState(false)
 
-  const minZoom = 1;
-  const maxZoom = 7;
-  const zoomStep = 0.5;
+  const [isAnimationTriggered, setIsAnimationTriggered] = useState(false)
+  const [isReverseAnimationTriggered, setIsReverseAnimationTriggered] =
+    useState(false)
+  const [stateView, setStateView] = useState([Math.PI / 2, 0, 0])
+  const [isToggleActive, setIsToggleActive] = useState(false)
+  const [isImageOpen, setIsImageOpen] = useState(false)
+  const [resetPosition, setResetPosition] = useState(false)
+  const [cameraPosition, setCameraPosition] = useState([0, 0, 5])
 
-  const getTypologyImage = (typology) => {
+  const minZoom = 15
+  const maxZoom = 85
+  const zoomStep = 5
+
+  const getTypologyImage = typology => {
     const typologyImages = {
       't-1': '/typologies images/TIPO-1.jpg',
       't-2': '/typologies images/TIPO-2.jpg',
@@ -43,107 +49,123 @@ function TypoPage({ activeModel, isLoaded, activeTypology }) {
       't-7': '/typologies images/TIPO-7.jpg',
       't-8': '/typologies images/TIPO-8.jpg',
       't-19': '/typologies images/TIPO-19.jpg',
-      't-23': '/typologies images/TIPO-23.jpg',
-    };
-    return typologyImages[typology] || '/typologies images/default.jpg';
-  };
-
-  useEffect(() => {
-    const handleWheel = (e) => {
-      e.preventDefault();
-      if (e.deltaY < 0) {
-        zoomIn();
-      } else {
-        zoomOut();
-      }
-    };
-    window.addEventListener('wheel', handleWheel, { passive: false });
-    return () => {
-      window.removeEventListener('wheel', handleWheel);
-    };
-  }, []);
-
-  const smoothZoom = (targetZoom) => {
-    const deltaZoom = targetZoom - zoom;
-    if (Math.abs(deltaZoom) < 0.01) {
-      setZoom(targetZoom);
-    } else {
-      const newZoom = zoom + deltaZoom * 0.1;
-      setZoom(newZoom);
-      requestAnimationFrame(() => smoothZoom(targetZoom));
+      't-23': '/typologies images/TIPO-23.jpg'
     }
-  };
+    return typologyImages[typology] || '/typologies images/default.jpg'
+  }
 
-  const rotateLeft = () => setRotation((prev) => prev + Math.PI / 8);
-  const rotateRight = () => setRotation((prev) => prev - Math.PI / 8);
+  const handleWheel = e => {
+    e.preventDefault()
+    if (rotateFront) {
+      if (e.deltaY < 0) {
+        setZoom2(prev => Math.min(Math.max(prev + 0.1, 0.6), 1.4))
+      } else {
+        setZoom2(prev => Math.min(Math.max(prev - 0.1, 0.6), 1.4))
+      }
+    } else {
+      if (e.deltaY < 0) {
+        setZoom(prev => Math.max(prev - zoomStep, minZoom))
+      } else {
+        setZoom(prev => Math.min(prev + zoomStep, maxZoom))
+      }
+    }
+  }
+
+  const smoothZoom = targetZoom => {
+    const deltaZoom = targetZoom - zoom
+    if (Math.abs(deltaZoom) < 0.01) {
+      setZoom(targetZoom)
+    } else {
+      const newZoom = zoom + deltaZoom * 0.1
+      setZoom(newZoom)
+      requestAnimationFrame(() => smoothZoom(targetZoom))
+    }
+  }
+
+  const rotateLeft = () => setRotation(prev => prev + Math.PI / 8)
+  const rotateRight = () => setRotation(prev => prev - Math.PI / 8)
   const zoomIn = () => {
-    const newZoom = Math.min(zoom + zoomStep, maxZoom);
-    smoothZoom(newZoom);
-  };
-
-  const zoomOut = () => {
-    const newZoom = Math.max(zoom - zoomStep, minZoom);
-    smoothZoom(newZoom);
-  };
-  const navigate = useNavigate();
-
-  const returnHome = () => {
-    navigate(-1);
-  };
-
-  const triggerAnimation = () => {
-    setIsAnimationTriggered(true);
-    setIsReverseAnimationTriggered(false);
-    setResetPosition(true);
-    setTimeout(() => {
-      setResetPosition(false);
-      toggleView();
-      rotateRight();
-      rotateRight();
-    }, 500);
-  };
-
-  const triggerResetPosition = () => {
-    setResetPosition(true);
-    setTimeout(() => setResetPosition(false), 500);
-  };
-
-  const triggerReverseAnimation = () => {
-    setIsReverseAnimationTriggered(true);
-    setIsAnimationTriggered(false);
-    setCameraPosition((prev) => (prev[1] === 0 ? [0, -5, 5] : [0, 0, 5]));
-    setIsToggleActive((prev) => !prev);
-
-    setStateView([Math.PI / 2, 0, 0]);
-    setResetPosition(true);
-    setTimeout(() => setResetPosition(false), 500);
-  };
+    if (rotateFront) {
+      setZoom2(prev => Math.min(Math.max(prev + 0.1, 0.6), 1.4))
+    } else {
+      setZoom(prev => Math.max(prev - zoomStep, minZoom))
+    }
+  }
 
   const toggleView = () => {
-    setCameraPosition((prev) => (prev[1] === 0 ? [0, -5, 5] : [0, 0, 5]));
-    setIsToggleActive((prev) => !prev);
-    setResetPosition(true);
-  };
+    setRotateFront(prev => !prev)
+    rotateFront === true ? setZoom(70) : setZoom2(0.8)
+  }
+
+  const zoomOut = () => {
+    if (rotateFront) {
+      setZoom2(prev => Math.min(Math.max(prev - 0.1, 0.6), 1.4))
+    } else {
+      setZoom(prev => Math.min(prev + zoomStep, maxZoom))
+    }
+  }
+  const navigate = useNavigate()
+
+  const returnHome = () => {
+    navigate(-1)
+  }
+
+  useEffect(() => {
+    console.log(activeModel.animations)
+
+    window.addEventListener('wheel', handleWheel, { passive: false })
+    return () => {
+      window.removeEventListener('wheel', handleWheel)
+    }
+  }, [rotateFront])
+
+  const triggerAnimation = () => {
+    setIsAnimationTriggered(true)
+    setIsReverseAnimationTriggered(false)
+    setResetPosition(true)
+    setTimeout(() => {
+      setResetPosition(false)
+      toggleView()
+      rotateRight()
+      rotateRight()
+    }, 500)
+  }
+
+  const triggerResetPosition = () => {
+    setResetPosition(true)
+    setTimeout(() => setResetPosition(false), 500)
+  }
+
+  const triggerReverseAnimation = () => {
+    setIsReverseAnimationTriggered(true)
+    setIsAnimationTriggered(false)
+    
+    setStateView([Math.PI / 2, 0, 0])
+    setResetPosition(true)
+    setRotateFront(false)
+    setIsToggleActive(prev => !prev)
+    setTimeout(() => setResetPosition(false), 500)
+  }
 
   const backIndex = () => {
-    navigate('/');
-  };
+    navigate('/')
+  }
 
   const openImageModal = () => {
-    setIsImageOpen(true);
-  };
+    setIsImageOpen(true)
+  }
 
   const closeImageModal = () => {
-    setIsImageOpen(false);
-  };
+    setIsImageOpen(false)
+  }
 
   return (
     <div>
-      <Canvas shadows>
+      <Canvas camera={{ fov: 15, position: [0, zoom, 0] }} shadows>
         <Suspense fallback={null}>
           <ambientLight intensity={0.8} />
           <directionalLight
-            color="#fade85"
+            color='#fade85'
             position={[10, 40, 20]}
             intensity={2}
             castShadow
@@ -167,10 +189,9 @@ function TypoPage({ activeModel, isLoaded, activeTypology }) {
             animations={activeModel.animations}
           />
           <CameraController
-            zoom={zoom} // Pasa el estado de zoom a CameraController
-            resetPosition={resetPosition}
-            isToggleActive={isToggleActive}
-            cameraPosition={cameraPosition}
+            zoom={zoom}
+            rotateFront={rotateFront}
+            zoom2={zoom2}
           />
         </Suspense>
       </Canvas>
@@ -182,16 +203,16 @@ function TypoPage({ activeModel, isLoaded, activeTypology }) {
             top: '20px',
             left: '20px',
             display: 'flex',
-            gap: '15px',
+            gap: '15px'
           }}
         >
           <AnimatedButton onClick={returnHome}>
-            <ReturnIcon width="30px" height="30px" />
+            <ReturnIcon width='30px' height='30px' />
           </AnimatedButton>
 
           {!isAnimationTriggered && (
             <AnimatedButton onClick={triggerAnimation}>
-              <View3dIcon width="30px" height="30px" />
+              <View3dIcon width='30px' height='30px' />
             </AnimatedButton>
           )}
 
@@ -199,34 +220,34 @@ function TypoPage({ activeModel, isLoaded, activeTypology }) {
             <>
               <AnimatedButton onClick={toggleView}>
                 {isToggleActive ? (
-                  <CameraUpIcon width="30px" height="30px" />
+                  <CameraUpIcon width='30px' height='30px' />
                 ) : (
-                  <CameraDownIcon width="30px" height="30px" />
+                  <CameraDownIcon width='30px' height='30px' />
                 )}
               </AnimatedButton>
               <AnimatedButton onClick={triggerReverseAnimation}>
-                <Hide3dIcon width="30px" height="30px" />
+                <Hide3dIcon width='30px' height='30px' />
               </AnimatedButton>
             </>
           )}
 
           <AnimatedButton onClick={triggerResetPosition}>
-            <FocusIcon width="30px" height="30px" />
+            <FocusIcon width='30px' height='30px' />
           </AnimatedButton>
 
           <AnimatedButton onClick={backIndex}>
-            <BuildingIcon width="30px" height="30px" />
+            <BuildingIcon width='30px' height='30px' />
           </AnimatedButton>
         </div>
       )}
 
       {isLoaded && (
-        <div className="menubar">
+        <div className='menubar'>
           <AnimatedButton
             style={{ display: 'flex', border: 'none', background: 'none' }}
             onClick={rotateLeft}
           >
-            <GlobalRotateIcon width="30px" height="30px" />
+            <GlobalRotateIcon width='30px' height='30px' />
           </AnimatedButton>
 
           <AnimatedButton
@@ -234,11 +255,11 @@ function TypoPage({ activeModel, isLoaded, activeTypology }) {
               display: 'flex',
               border: 'none',
               background: 'none',
-              color: 'white',
+              color: 'white'
             }}
             onClick={zoomOut}
           >
-            <ZoomOutIcon width="30px" height="30px" />
+            <ZoomOutIcon width='30px' height='30px' />
           </AnimatedButton>
 
           <AnimatedButton
@@ -246,11 +267,11 @@ function TypoPage({ activeModel, isLoaded, activeTypology }) {
               display: 'flex',
               border: 'none',
               background: 'none',
-              color: 'white',
+              color: 'white'
             }}
             onClick={zoomIn}
           >
-            <ZoomInIcon width="30px" height="30px" />
+            <ZoomInIcon width='30px' height='30px' />
           </AnimatedButton>
 
           <AnimatedButton
@@ -258,8 +279,8 @@ function TypoPage({ activeModel, isLoaded, activeTypology }) {
             onClick={rotateRight}
           >
             <GlobalRotateIcon
-              width="30px"
-              height="30px"
+              width='30px'
+              height='30px'
               style={{ transform: 'scaleX(-1)' }}
             />
           </AnimatedButton>
@@ -310,4 +331,4 @@ function TypoPage({ activeModel, isLoaded, activeTypology }) {
   )
 }
 
-export default TypoPage;
+export default TypoPage
